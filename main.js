dd57fb6d5f6951ed99361a6cf52de54de1fd8b63
migiiiii <manicdesign.lab@gmail.com>
2020-05-25 16:20:15 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-import UserAgentElement from './components/user-agent.js';
-import WidthSizeElement from './components/width-size.js';
+import UserAgentElement from './components/user-agent.js'
+import WidthSizeElement from './components/width-size.js'
 
-customElements.define('user-agent', UserAgentElement);
-customElements.define('width-size', WidthSizeElement);
+customElements.define('user-agent', UserAgentElement)
+customElements.define('width-size', WidthSizeElement)
 
